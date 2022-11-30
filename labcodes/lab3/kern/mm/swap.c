44bcad813eb93ba6b2b958b3c11ff85b58b3c686
ArcanusNEO <lucasding@foxmail.com>
2022-11-30 23:12:21 +0800

    fix

--- labcodes/lab3/kern/mm/swap.c
+++ labcodes/lab3/kern/mm/swap.c
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <swap.h>
-#include <swap_fifo.h>
+#include <swap_clock.h>
 #include <swapfs.h>
 
 // the valid vaddr for check is between 0~CHECK_VALID_VADDR-1
@@ -34,7 +34,7 @@ int swap_init(void) {
     panic("bad max_swap_offset %08x.\n", max_swap_offset);
   }
 
-  sm = &swap_manager_fifo;
+  sm = &swap_manager_clock;
   int r = sm->init();
 
   if (r == 0) {
